0019ecf8201f595baf3bea73d7344c6bec8d153a
alxsaunders <alexsaunders242@gmail.com>
2022-09-06 05:43:59 -0400

    fix productlist

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,20 +1,30 @@
+// import react dependencies
 import React, { useEffect } from 'react';
-import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { useDispatch, useSelector } from 'react-redux';
+
+// import apollo dependency
 import { useQuery } from '@apollo/client';
+
+// import utils dependencies
+import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
+
+// import component
+import ProductItem from '../ProductItem';
+
+// import assset
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
-
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
   const { currentCategory } = state;
-
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
+  // if data, loading, or dispatch is updated, update products
   useEffect(() => {
+    // retrieved from server
     if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
@@ -23,7 +33,9 @@ function ProductList() {
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
       });
-    } else if (!loading) {
+    } 
+    // get cache from idb
+    else if (!loading) {
       idbPromise('products', 'get').then((products) => {
         dispatch({
           type: UPDATE_PRODUCTS,
@@ -33,6 +45,7 @@ function ProductList() {
     }
   }, [data, loading, dispatch]);
 
+  // return products if currentCategory does not exist, then filter and return products that match product.category._id
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
