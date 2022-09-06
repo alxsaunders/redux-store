1ccd35d0729990dc46768ffcfb78b12e5cb6d2a0
alxsaunders <alexsaunders242@gmail.com>
2022-09-06 05:39:59 -0400

    fix category

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,21 +1,24 @@
+// import react dependencies
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+// import apollo dependency
 import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
-import {
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
-} from '../../utils/actions';
+
+// import utils dependencies
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
-
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
   const { categories } = state;
-
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
+  // if categoryData, loading, or dispatch is updated, update category
   useEffect(() => {
+    // retrieved from server
     if (categoryData) {
       dispatch({
         type: UPDATE_CATEGORIES,
@@ -24,7 +27,9 @@ function CategoryMenu() {
       categoryData.categories.forEach((category) => {
         idbPromise('categories', 'put', category);
       });
-    } else if (!loading) {
+    } 
+    // get cache from idb
+    else if (!loading) {
       idbPromise('categories', 'get').then((categories) => {
         dispatch({
           type: UPDATE_CATEGORIES,
@@ -34,6 +39,7 @@ function CategoryMenu() {
     }
   }, [categoryData, loading, dispatch]);
 
+  // update current category when button is clicked
   const handleClick = (id) => {
     dispatch({
       type: UPDATE_CURRENT_CATEGORY,
